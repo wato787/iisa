faf2a73b2fc6ec2c0138f15e844e5f5dbccc61c5
wato787 <wato787@icloud.com>
2023-08-30 22:55:44 +0900

    fix logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,11 @@ import Auth from "./Auth";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  // const { status } = useSession();
+  const { status } = useSession();
 
   return (
     <div className="h-screen w-full bg-blue-300">
-      <Image
-        src="/iisaLogo.png"
-        width={250}
-        height={250}
-        alt="積み上げいいさ"
-      />
+      <Image src="/logoo.png" width={250} height={250} alt="積み上げいいさ" />
       {/* <Auth /> */}
     </div>
   );
