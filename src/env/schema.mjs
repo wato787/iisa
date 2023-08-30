8826096eaffc4cf5a3800f6b6446c89a68515e95
wato787 <wato787@icloud.com>
2023-08-30 22:52:55 +0900

    delete node_env

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,7 +7,6 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
-  NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.string().url(),
   GOOGLE_CLIENT_ID: z.string(),
