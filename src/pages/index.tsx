329b67a6f0e271fd856b3d8ed80c4b9b7bb04fe2
wato787 <wato787@icloud.com>
2023-08-30 20:53:56 +0900

    eslint　ミス

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
 
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Auth from "./Auth";
 
 const Home: NextPage = () => {
+  const { data: session, status } = useSession();
   return (
     <>
       <Auth />
