1c15595c2d8a555777199c6cbfc3c4df1e3b9ea8
wato787 <wato787@icloud.com>
2023-08-31 14:47:06 +0900

    delete env

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,7 +2,6 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { appRouter } from "../../../server/trpc/router";
 import { createContext } from "../../../server/trpc/context";
-import { env } from "../../../env/server.mjs";
 
 // export API handler
 export default createNextApiHandler({
