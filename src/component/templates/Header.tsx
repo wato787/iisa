11c140bfe9332af6d87a796df28819a9c01f65f7
ck <shibaduke4@gmail.com>
2023-09-02 22:27:41 +0900

    時間追加

---
+++ src/component/templates/Header.tsx
@@ -0,0 +1,18 @@
+import { signOut } from "next-auth/react";
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <ul className="flex">
+        <li className="m-10">ホーム</li>
+        <li className="m-10">グラフ</li>
+        <button   onClick={() =>
+          signOut({ callbackUrl: "http://localhost:3000/auth/signin" })
+        } className="m-10">ログアウト</button>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 import type { GetServerSideProps, NextPage } from "next";
 import { getSession, signOut, useSession } from "next-auth/react";
+import Header from "../component/templates/Header";
+import TimeCard from "../component/card/TimeCard";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
   console.log(session);
   return (
-    <>
+    <div className="h-screen  bg-primary flex flex-col items-center justify-center">
+      <Header />
+      <TimeCard />
       <button
         onClick={() =>
           signOut({ callbackUrl: "http://localhost:3000/auth/signin" })
@@ -14,7 +18,7 @@ const Home: NextPage = () => {
       >
         logout
       </button>
-    </>
+    </div>
   );
 };
 
